393e397e32be1b15e2353f201b191d9cc6890b68
abbat01 <95858072+abbat01@users.noreply.github.com>
2022-06-15 00:07:45 +0300

    feat: fix bugs with i content (Wen, 15.06 0:10:22)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 const telegram = document.querySelector('.fa-telegram-plane');
 
-telegram.addEventListener('mouseout', function() {
+telegram.addEventListener('click', function() {
     const div = document.createElement('div')
-    div.style.backgroundColor = 'red'
-    div.textContent = 'Hi!'
+    div.style.backgroundColor = 'rgba(125, 125, 125, 0.4)'
+    div.textContent = 'abbat002'
     telegram.append(div)
-}, { once: true })
+}, { once: true });
